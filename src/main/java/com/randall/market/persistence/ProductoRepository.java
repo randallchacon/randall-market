425096db016ae4e11c89798650fcc9ee21ca8ff7
Randall Chacon <info@randallchacon.com>
2022-04-20 10:59:59 -0600

    modificaciones a clase product repository y crud para agregar query methods c19

--- src/main/java/com/randall/market/persistence/ProductoRepository.java
+++ src/main/java/com/randall/market/persistence/ProductoRepository.java
@@ -4,12 +4,11 @@ import com.randall.market.persistence.crud.ProductoCrudRepository;
 import com.randall.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
-
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
-    }
-
+    public List<Producto> getAll(){ return (List<Producto>) productoCrudRepository.findAll();}
+    public List<Producto> getByCategoria(int idCategoria){ return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);}
+    public Optional<List<Producto>> getEscasos(int cantidad){ return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);}
 }
--- src/main/java/com/randall/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/randall/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,14 @@
 package com.randall.market.persistence.crud;
 
 import com.randall.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> { //define la clase y el tipo de dato del PK
+    //@Query(value = "SELECT * FROM producto WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
